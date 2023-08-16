dd68c0bc3e677ed3898bb87a0f56bffc7b6e077e
Rodolfo Hercule <71905257+rodolfohercules@users.noreply.github.com>
2023-08-16 16:30:42 -0300

    firstCommit

---
+++ app.js
@@ -0,0 +1,24 @@
+/*
+
+Irei identificar os tipos de cada variável:
+
+nome: 'String';
+salário: 'Number or Float';
+idade: 'Number';
+possuiDiploma: 'Boolean'.
+
+*/
+
+const pessoa = {
+
+    nome: 'Rodolfo Hercule',
+    salario: 2000.50,
+    idade: 34,
+    possuiDiploma: True
+
+};
+
+console.log('Nome:', pessoa.nome);
+console.log('Salário:', pessoa.salario);
+console.log('Idade:', pessoa.idade);
+console.log('Possui Diploma:', pessoa.possuiDiploma);
